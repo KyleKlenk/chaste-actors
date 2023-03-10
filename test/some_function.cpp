917925fb59a7f546cb445a33eb90514718196c15
Kyle <kyle.c.klenk@gmail.com>
2023-03-10 18:50:53 +0000

    add struct for parameters

--- test/some_function.cpp
+++ test/some_function.cpp
@@ -3,7 +3,7 @@
 using namespace caf;
 using json = nlohmann::json;
 
-int read_json_file() {
+int read_json_file(parameters& p) {
   std::ifstream fin("/code/Chaste/projects/chaste-actors/test/parameters.json");
 
   // Check if the file was opened successfully
@@ -17,72 +17,39 @@ int read_json_file() {
   fin >> j;
 
   // Read in parameters
-  int numDigits = j["numDigits"];
-  int numTimeSteps = j["numTimeSteps"];
-  std::string dtTissue = j["dtTissue"];
-  std::string dtCell = j["dtCell"];
-  std::string mesh = j["mesh"];
-  double timeDuration = j["timeDuration"];
-  std::string stimulusCurrent = j["stimulusCurrent"];
-  double stimulusDuration = j["stimulusDuration"];
-  double stimulusStarttime = j["stimulusStarttime"];
-  double stimulusRange_x = j["stimulusRange_x"];
-  double stimulusRange_y = j["stimulusRange_y"];
-  double stimulusRange_z = j["stimulusRange_z"];
-  double electrodeposition_x = j["electrodeposition_x"];
-  double electrodeposition_y = j["electrodeposition_y"];
-  double electrodeposition_z = j["electrodeposition_z"];
-  double sigma_ix = j["sigma_ix"];
-  double sigma_iy = j["sigma_iy"];
-  double sigma_iz = j["sigma_iz"];
-  double sigma_ex = j["sigma_ex"];
-  double sigma_ey = j["sigma_ey"];
-  double sigma_ez = j["sigma_ez"];
-  int Cm = j["Cm"];
-  int Chi = j["Chi"];
-  double xLength = j["xLength"];
-  double yLength = j["yLength"];
-  double zLength = j["zLength"];
-  std::string cellModel = j["cellModel"];
-  std::string cellModelfileName = j["cellModelfileName"];
-  std::string cellModelConstructor=  j ["cellModelConstructor"]; 
-	std::string odeSolver= 	j ["odeSolver"];
+  p.numDigits = j["numDigits"];
+  p.numTimeSteps = j["numTimeSteps"];
+  p.dtTissue = j["dtTissue"];
+  p.dtCell = j["dtCell"];
+  p.mesh = j["mesh"];
+  p.timeDuration = j["timeDuration"];
+  p.stimulusCurrent = j["stimulusCurrent"];
+  p.stimulusDuration = j["stimulusDuration"];
+  p.stimulusStarttime = j["stimulusStarttime"];
+  p.stimulusRange_x = j["stimulusRange_x"];
+  p.stimulusRange_y = j["stimulusRange_y"];
+  p.stimulusRange_z = j["stimulusRange_z"];
+  p.electrodeposition_x = j["electrodeposition_x"];
+  p.electrodeposition_y = j["electrodeposition_y"];
+  p.electrodeposition_z = j["electrodeposition_z"];
+  p.sigma_ix = j["sigma_ix"];
+  p.sigma_iy = j["sigma_iy"];
+  p.sigma_iz = j["sigma_iz"];
+  p.sigma_ex = j["sigma_ex"];
+  p.sigma_ey = j["sigma_ey"];
+  p.sigma_ez = j["sigma_ez"];
+  p.Cm = j["Cm"];
+  p.Chi = j["Chi"];
+  p.xLength = j["xLength"];
+  p.yLength = j["yLength"];
+  p.zLength = j["zLength"];
+  p.cellModel = j["cellModel"];
+  p.cellModelfileName = j["cellModelfileName"];
+  p.cellModelConstructor=  j ["cellModelConstructor"]; 
+	p.odeSolver= 	j ["odeSolver"];
 
-  std::cout << "numDigits: " << numDigits << std::endl;
-  std::cout << "numTimeSteps: " << numTimeSteps << std::endl;
-  std::cout << "dtTissue: " << dtTissue << std::endl;
-  std::cout << "dtCell: " << dtCell << std::endl;
-  std::cout << "mesh: " << mesh << std::endl;
-  std::cout << "timeDuration: " << timeDuration << std::endl;
-  std::cout << "stimulusCurrent: " << stimulusCurrent << std::endl;
-  std::cout << "stimulusDuration: " << stimulusDuration << std::endl;
-  std::cout << "stimulusStarttime: " << stimulusStarttime << std::endl;
-  std::cout << "stimulusRange_x: " << stimulusRange_x << std::endl;
-  std::cout << "stimulusRange_y: " << stimulusRange_y << std::endl;
-  std::cout << "stimulusRange_z: " << stimulusRange_z << std::endl;
-  std::cout << "electrodeposition_x: " << electrodeposition_x << std::endl;
-  std::cout << "electrodeposition_y: " << electrodeposition_y << std::endl;
-  std::cout << "electrodeposition_z: " << electrodeposition_z << std::endl;
-  std::cout << "sigma_ix: " << sigma_ix << std::endl;
-  std::cout << "sigma_iy: " << sigma_iy << std::endl;
-  std::cout << "sigma_iz: " << sigma_iz << std::endl;
-  std::cout << "sigma_ex: " << sigma_ex << std::endl;
-  std::cout << "sigma_ey: " << sigma_ey << std::endl;
-  std::cout << "sigma_ez: " << sigma_ez << std::endl;
-  std::cout << "Cm: " << Cm << std::endl;
-  std::cout << "Chi: " << Chi << std::endl;
-  std::cout << "xLength: " << xLength << std::endl;
-  std::cout << "yLength: " << yLength << std::endl;
-  std::cout << "zLength: " << zLength << std::endl;
-  std::cout << "cellModel: " << cellModel << std::endl;
-  std::cout << "cellModelfileName: " << cellModelfileName << std::endl;
-  std::cout << "cellModelConstructor: " << cellModelConstructor << std::endl;
-  std::cout << "odeSolver: " << odeSolver << std::endl;
-
-
-
-	// Use the variables in your program
-	// ...
+  // Close the file
+  fin.close();
 
 	return 0;
 }
@@ -90,7 +57,11 @@ int read_json_file() {
 
 void caf_main(actor_system& system) {
   scoped_actor self{system};
-  read_json_file();
+
+  parameters p;
+
+  read_json_file(p);
+  
   aout(self) << "Hello World From Actor Land" << std::endl;
 
 }
--- test/some_function.hpp
+++ test/some_function.hpp
@@ -6,7 +6,40 @@
 #include <fstream>
 
 
-int read_json_file();
+struct parameters {
+  int numDigits;
+  int numTimeSteps;
+  std::string dtTissue;
+  std::string dtCell;
+  std::string mesh;
+  double timeDuration;
+  std::string stimulusCurrent;
+  double stimulusDuration;
+  double stimulusStarttime;
+  double stimulusRange_x;
+  double stimulusRange_y;
+  double stimulusRange_z;
+  double electrodeposition_x;
+  double electrodeposition_y;
+  double electrodeposition_z;
+  double sigma_ix;
+  double sigma_iy;
+  double sigma_iz;
+  double sigma_ex;
+  double sigma_ey;
+  double sigma_ez;
+  int Cm;
+  int Chi;
+  double xLength;
+  double yLength;
+  double zLength;
+  std::string cellModel;
+  std::string cellModelfileName;
+  std::string cellModelConstructor;
+  std::string odeSolver;
+};
+
+int read_json_file(parameters &p);
 
 /**
  * Is the entry point of the actor system
